a3062a51c2ed7738402a469fcadc9ce6d8343a3d
jefzli21 <jefzli21@gmail.com>
2022-10-21 22:03:15 -0700

    formlabelscomplete

--- frontend/src/components/BusinessCreateForm/index.js
+++ frontend/src/components/BusinessCreateForm/index.js
@@ -11,19 +11,47 @@ function BusinessCreateForm(){
         
         <form>
             <label>
-                
+            Business Name:
             <input type="text" />
             </label>
 
             <label>
+            Business Type:
             <input type="text" />
             </label>
 
             <label>
+            Address:
             <input type="text" />
             </label>
 
             <label>
+            Hours:
+            <input type="text" />
+            </label>
+
+            <label>
+            About:
+            <input type="text" />
+            </label>
+
+            <label>
+            Features:
+            <input type="text" />
+            </label>
+
+            <label>
+            phone:
+            <input type="text" />
+            </label>
+
+            <label>
+            Location Longitude:
+            <input type="text" />
+            </label>
+
+            <label>
+            Location Latitude:
             <input type="text" />
             </label>
         </form>
